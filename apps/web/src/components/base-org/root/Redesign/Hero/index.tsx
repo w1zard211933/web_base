930e36da63ffeceeb630ec46b98922908d82a794
Ryan Scheuer <ryan.scheuer1@gmail.com>
2025-07-25 15:19:00 -0400

    Fix: Smaller Hero.tsx

--- apps/web/src/components/base-org/root/Redesign/Hero/index.tsx
+++ apps/web/src/components/base-org/root/Redesign/Hero/index.tsx
@@ -1,14 +1,14 @@
 import Container from 'apps/web/src/components/base-org/Container';
 import AsciiFluidScene from 'apps/web/src/components/WebGL/Scenes/AsciiFluidScene';
-import defaultImg from 'apps/web/public/images/backgrounds/default.webp';
-import spaceImg from 'apps/web/public/images/backgrounds/space2.webp';
+// import defaultImg from 'apps/web/public/images/backgrounds/default.webp';
+// import spaceImg from 'apps/web/public/images/backgrounds/space2.webp';
 import spaceCropImg from 'apps/web/public/images/backgrounds/space-crop.webp';
 import { AnimatedTitle } from 'apps/web/src/components/base-org/root/Redesign/Hero/AnimatedTitle';
 
 export function Hero() {
   return (
-    <Container className="grid-cols-9 gap-y-12 lg:pt-0">
-      <div className="relative col-span-full h-[412px] w-full pb-0 lg:h-[670px] xl:h-[870px]">
+    <Container className="!lg:mb-0 !mb-0 grid-cols-9 gap-y-12 lg:pt-0">
+      <div className="relative col-span-full h-[412px] w-full pb-0 lg:h-[40vh] xl:h-[40vh]">
         <AnimatedTitle />
 
         <div className="absolute inset-0">
