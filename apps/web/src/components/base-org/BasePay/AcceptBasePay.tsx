43407b9b03fa6b930b3b78c3f089465c48fa6e16
Ryan Scheuer <ryan.scheuer1@gmail.com>
2025-08-02 10:05:58 -0700

    Update titles

--- apps/web/src/components/base-org/BasePay/AcceptBasePay.tsx
+++ apps/web/src/components/base-org/BasePay/AcceptBasePay.tsx
@@ -2,32 +2,28 @@
 
 import Title from 'apps/web/src/components/base-org/typography/TitleRedesign';
 import { TitleLevel } from 'apps/web/src/components/base-org/typography/TitleRedesign/types';
-import {
-  Button,
-  ButtonVariants,
-  ButtonSizes,
-} from 'apps/web/src/components/Button/Redesign/Button';
 import Link from 'apps/web/src/components/Link';
+import AnimatedButton from 'apps/web/src/components/Button/AnimatedButton';
 
 export default function AcceptBasePay() {
   return (
     <section className="col-span-full py-12 text-black">
-      <div className="flex flex-col items-center justify-between gap-12 md:flex-row md:gap-24">
-        <Title level={TitleLevel.H4Regular}>Accept Base Pay on your online store</Title>
-        <div className="flex flex-col gap-8">
-          <Title level={TitleLevel.H5Regular}>
+      <div className="flex flex-col gap-12 justify-between items-center md:flex-row md:gap-24">
+        <div className="">
+          <Title level={TitleLevel.H1Regular}>Accept Base Pay on your online store</Title>
+        </div>
+        <div className="flex flex-col gap-8 md:w-1/2 lg:w-3/5">
+          <Title level={TitleLevel.H2Regular}>
             Base Pay works on stores running Shopify payments. You can also accept Base Pay by using
             our SDK.
           </Title>
-          <Button variant={ButtonVariants.Primary} size={ButtonSizes.Small} className="w-1/2 px-2">
-            <Link
-              href="https://docs.base.org/base-account/guides/accept-payments"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Accept Base Pay
-            </Link>
-          </Button>
+          <Link
+            href="https://docs.base.org/base-account/guides/accept-payments"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <AnimatedButton text="Accept Base Pay" />
+          </Link>
         </div>
       </div>
     </section>
