9e649fd766194f9a9eb754fae9604f2b31d1cd49
xtbase <teaonbase@gmail.com>
2025-10-14 16:29:11 -0700

    chore: dummy commit remove comment (#2717)

--- apps/web/src/components/base-org/shared/RenderOnInView.tsx
+++ apps/web/src/components/base-org/shared/RenderOnInView.tsx
@@ -7,7 +7,6 @@ export default function RenderOnInView({ children }: { children: React.ReactNode
   const [isVisible, setIsVisible] = useState(false);
 
   useEffect(() => {
-    // test
     if (!ref.current) return;
 
     const observer = new IntersectionObserver(
